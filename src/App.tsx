dd2101990c944f1328480c4abf02a4d8cba76d21
NatalieHarrison <xnataaliee@aol.com>
2022-05-28 17:07:37 -0400

    Textfield and button
    User will enter a equation in the text field and hit button when equation is entered

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
+import { Box, TextField, Button, Stack } from "@mui/material";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Box>
+        <Stack spacing={2} direction="row">
+          <TextField id="equation" variant="outlined" />
+          <Button variant="contained"> Evaluate </Button>
+        </Stack>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
