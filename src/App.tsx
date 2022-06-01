f0ce19a0f209cc9fd766dd86a37e6d381e0f0b29
NatalieHarrison <xnataaliee@aol.com>
2022-06-01 17:05:00 -0400

    tracking input and math.evaluate
    Set the state of input to be whatever the user types in until they are done typing. When the "evaluate" button is clicked, the equation the user inputted is evaluated in the "handleClick" function but not outputted.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Box, TextField, Button, Stack } from "@mui/material";
+import { Box } from "@mui/material";
 import { Evaluate } from "./components/Evaluate";
-import { InputEquation } from "./components/InputEquation";
 
 function App() {
   return (
     <div className="App">
       <Box>
-        <Stack spacing={2} direction="row">
-          <InputEquation></InputEquation>
-          <Evaluate />
-        </Stack>
+        <Evaluate></Evaluate>
       </Box>
     </div>
   );
--- src/components/Evaluate.tsx
+++ src/components/Evaluate.tsx
@@ -1,12 +1,24 @@
-import { Button } from "@mui/material";
-import { InputEquation } from "./components/InputEquation";
-const handleClick = () => {
-  console.log("Hello World");
-};
+import { TextField, Button, Stack, Input } from "@mui/material";
+import React, { useState } from "react";
+import { create, all } from "mathjs";
+
 export const Evaluate = () => {
+  const math = create(all);
+
+  const [input, setInput] = useState("");
+  const handleClick = () => {
+    math.evaluate(input);
+  };
   return (
-    <Button onClick={handleClick} variant="contained">
-      Evaluate
-    </Button>
+    <Stack spacing={2} direction="row">
+      <TextField
+        id="equation"
+        variant="outlined"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <Button onClick={handleClick} variant="contained">
+        Evaluate
+      </Button>
+    </Stack>
   );
 };
